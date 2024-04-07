78e9b25d3d7cc3018993a75017756a3b6ea0c9d5
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2024-04-07 21:12:10 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("video music Bling-Bang-Bang-Born ");
+});
+
+audio.addEventListener("click", function audio(){
+    alert(" Audio Bling-Bang-Bang-Born Video");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert(" Gambar Mashel");
+});
